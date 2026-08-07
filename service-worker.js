9d597cc62d9d123f2eb1bef1952b4704ade34a0b
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-07 20:43:04 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='apprentice-plus-beta-v2-1-shared-photo-video-evidence';
+const CACHE='apprentice-plus-beta-v2-1-assignment-otj-evidence';
 const CURRENT_VERSION='V2.1';
 const RELEASE_NOTES_URL='./release-notes.json';
 const FALLBACK_UPDATE_INFO={
